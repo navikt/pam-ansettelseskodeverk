76061e036e36848ead75ff99924dc279c68424b5
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-02-11 12:22:40 +0100

    tag the release version

--- release.sh
+++ release.sh
@@ -6,8 +6,9 @@ COMMIT=$(git rev-parse --short=8 HEAD)
 VERSION="1.$TIME-$COMMIT"
 echo "Setting version $VERSION"
 
-mvn -B versions:set -DnewVersion="$VERSION"
-mvn -B versions:commit
+#mvn -B versions:set -DnewVersion="$VERSION"
+#mvn -B versions:commit
 
-echo "Running release"
-mvn -B --settings maven-settings.xml deploy -Dmaven.wagon.http.pool=false
+#echo "Running release"
+#mvn -B --settings maven-settings.xml deploy -Dmaven.wagon.http.pool=false
+echo $VERSION > .version_tag
